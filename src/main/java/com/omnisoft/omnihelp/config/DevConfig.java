965e12ccbd8bd53e45360c3f1c842bb6229a99b9
Felipe Lima <felipelima1910@gmail.com>
2023-05-19 18:52:39 -0300

    Developer profile, connection db postgre

---
+++ src/main/java/com/omnisoft/omnihelp/config/DevConfig.java
@@ -0,0 +1,29 @@
+package com.omnisoft.omnihelp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.omnisoft.omnihelp.services.DBServices;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+    
+    @Autowired
+    private DBServices dbServices;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+
+    @Bean
+    public boolean instanceDB() {
+        if(value.equals("create")) {
+            this.dbServices.instanceDB();
+        }
+        return false;
+    }
+
+}
