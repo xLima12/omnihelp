558442e7b5526c0126df5ef5730f1f622881e487
Felipe Lima <felipelima1910@gmail.com>
2023-05-19 13:26:48 -0300

    omnihelp

---
+++ src/main/java/com/omnisoft/omnihelp/omnihelp/OmnihelpApplication.java
@@ -0,0 +1,13 @@
+package com.omnisoft.omnihelp.omnihelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnihelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnihelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omnisoft/omnihelp/omnihelp/OmnihelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnisoft.omnihelp.omnihelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnihelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
